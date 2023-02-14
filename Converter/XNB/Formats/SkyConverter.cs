e90e19e0d001b7f2efa7fec581beb7562213ef1d
Krzyhau <krzyht@gmail.com>
2023-02-14 21:49:34 +0100

    simplify sky converter

--- Converter/XNB/Formats/SkyConverter.cs
+++ Converter/XNB/Formats/SkyConverter.cs
@@ -1,12 +1,7 @@
 ﻿using System.Text;
 
-using FEZRepacker.Converter.Definitions.FezEngine;
 using FEZRepacker.Converter.Definitions.FezEngine.Structure;
-using FEZRepacker.Converter.Definitions.FezEngine.Structure.Input;
-using FEZRepacker.Converter.Definitions.FezEngine.Structure.Scripting;
-using FEZRepacker.Converter.FileSystem;
 using FEZRepacker.Converter.XNB.Formats.Json;
-using FEZRepacker.Converter.XNB.Formats.Json.CustomStructures;
 using FEZRepacker.Converter.XNB.Types;
 using FEZRepacker.Converter.XNB.Types.System;
 
@@ -14,7 +9,7 @@ using static FEZRepacker.Converter.XNB.Formats.Json.CustomStructures.ModifiedMap
 
 namespace FEZRepacker.Converter.XNB.Formats
 {
-    internal class SkyConverter : XnbFormatConverter
+    internal class SkyConverter : JsonStorageConverter<Sky>
     {
         public override List<XnbContentType> TypesFactory => new List<XnbContentType>
         {
@@ -26,26 +21,5 @@ namespace FEZRepacker.Converter.XNB.Formats
         };
 
         public override string FileFormat => ".fezsky";
-
-        public override FileBundle ReadXNBContent(BinaryReader xnbReader)
-        {
-            XnbContentType primaryType = PrimaryContentType;
-
-            Sky data = (Sky)primaryType.Read(xnbReader);
-
-            var json = CustomJsonSerializer.Serialize(data);
-
-            var outStream = new MemoryStream(Encoding.UTF8.GetBytes(json));
-            return FileBundle.Single(outStream, FileFormat);
-        }
-
-        public override void WriteXnbContent(FileBundle bundle, BinaryWriter xnbWriter)
-        {
-            var inReader = new BinaryReader(bundle.GetData());
-            string json = new string(inReader.ReadChars((int)inReader.BaseStream.Length));
-            var nodeDict = CustomJsonSerializer.Deserialize<Sky>(json);
-
-            PrimaryContentType.Write(nodeDict, xnbWriter);
-        }
     }
 }
