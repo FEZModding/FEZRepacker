fcfed43d2098dd58a2551622ad82bfe31ea96958
Krzyhau <krzyht@gmail.com>
2024-01-10 23:36:03 +0100

    isolate version to core project file

--- Interface/Program.cs
+++ Interface/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.Globalization;
 
+using FEZRepacker.Converter.XNB;
+
 namespace FEZRepacker.Interface
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string version = typeof(XnbConverter).Assembly.GetName().Version?.ToString() ?? "";
+            version = string.Join(".", version.Split('.').Take(3));
             // showoff
-            Console.WriteLine(
-                "================================================\n" +
-                "= FEZRepacker 0.4 by Krzyhau & FEZModding Team =\n" +
-                "================================================\n"
-            );
+            Console.WriteLine($"=== FEZRepacker {version} by Krzyhau & FEZModding Team ===\n");
 
             // keep number decimals consistent
             Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-GB");
