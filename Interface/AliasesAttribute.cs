bec73648e29a407cc45143e81461c90bee28e249
zerocker <zerocloude@gmail.com>
2025-10-14 16:08:18 +0900

    add custom attribute and parser for enum CLI arguments

---
+++ Interface/AliasesAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace FEZRepacker.Interface
+{
+    /// <summary>
+    /// Used to store aliases.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    internal class AliasesAttribute(params string[] aliases) : Attribute
+    {
+        public string[] Aliases { get; } = aliases;
+    }
+}
