26ada001497490b6dfb2a217a1da549dfe3b288e
Chris Hausky <contact@krzyhau.pl>
2026-06-02 00:32:27 +0200

    chore: reformat text storage class

--- Core/Definitions/Game/Helpers/TextStorage.cs
+++ Core/Definitions/Game/Helpers/TextStorage.cs
@@ -1,18 +1,22 @@
+using FEZRepacker.Core.Helpers;
+
 namespace FEZRepacker.Core.Definitions.Game.Helpers
 {
-    [XnbReaderType(
-        "Microsoft.Xna.Framework.Content.DictionaryReader`2[[" +
-                    "System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]," +
-                    "[System.Collections.Generic.Dictionary`2[[" +
-                        "System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]," +
-                        "[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" +
-                    "]], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089" +
-                "]]")]
-    // Wrapper class for text storage asset type. Helps with its management.
-    // Original game reads dictionary type directly.
+    /// <summary>
+    /// Wrapper class for text storage asset type. Helps with its management.
+    /// Original game reads dictionary type directly.
+    /// </summary>
+    [XnbReaderType("Microsoft.Xna.Framework.Content.DictionaryReader`2[" +
+                   "[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]," +
+                   "[System.Collections.Generic.Dictionary`2[" +
+                   "[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]," +
+                   "[System.String, mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]" +
+                   "], mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089]]")]
+    
     public class TextStorage
     {
         [XnbProperty(SkipIdentifier = true, UseConverter = true)]
-        public IDictionary<string, IDictionary<string, string>> AllResources { get; set; }
+        public IDictionary<string, IDictionary<string, string>> AllResources { get; set; } =
+            new OrderedDictionary<string, IDictionary<string, string>>();
     }
 }
